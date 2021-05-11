9d4dcab75111314529a473ebe8da614d56661067
Alex Moran <alexmoran@lbl.gov>
2021-05-11 12:58:02 -0700

    revert

--- deploy.sh
+++ deploy.sh
@@ -97,70 +97,70 @@ fi
 cd $DIR
 
 # build a local model database
-# if [[ $MODELS ]]
-# then
-# Temporal directory for the repository
-  TMP_DIR=`mktemp -d`
-  cd $TMP_DIR
-
-# If no arg given then download from gazebo_models repo
-if [[ -z $LOCAL ]]
+if [[ $MODELS ]]
 then
-  echo -n "Downloading gazebo_models..."
-   # git clone https://github.com/osrf/gazebo_models -b master
-  git clone https://github.com/lbl-anp/gazebo_models -b master
+  # Temporal directory for the repository
+    TMP_DIR=`mktemp -d`
+    cd $TMP_DIR
 
-  RETVAL=$?
-  if [ $RETVAL -ne 0 ]; then
-    echo There are mercurial clone errors, exiting.
-    exit 1
+  # If no arg given then download from gazebo_models repo
+  if [[ -z $LOCAL ]]
+  then
+    echo -n "Downloading gazebo_models..."
+    # git clone https://github.com/osrf/gazebo_models -b master
+    git clone https://github.com/lbl-anp/gazebo_models -b master
+
+    RETVAL=$?
+    if [ $RETVAL -ne 0 ]; then
+      echo There are mercurial clone errors, exiting.
+      exit 1
+    fi
+
+    echo "Download complete"
+    cd gazebo_models
+    mkdir build
+    cd build
+    echo -n "Installing gazebo_models..."
+    cmake .. -DCMAKE_INSTALL_PREFIX=$DIR/http/client && make install > /dev/null 2>&1
+
+    RETVAL=$?
+    if [ $RETVAL -ne 0 ]; then
+      echo There are build errors, exiting.
+      exit 1
+    fi
+
+    echo "Install complete"
+
+    # Remove temp dir
+    rm -rf $TMP_DIR
+    rm -rf $DIR/http/client/assets
+    mv $DIR/http/client/models $DIR/http/client/assets
   fi
 
-  echo "Download complete"
-  cd gazebo_models
-  mkdir build
-  cd build
-  echo -n "Installing gazebo_models..."
-  cmake .. -DCMAKE_INSTALL_PREFIX=$DIR/http/client && make install > /dev/null 2>&1
+  cd $DIR
 
-  RETVAL=$?
-  if [ $RETVAL -ne 0 ]; then
-    echo There are build errors, exiting.
-    exit 1
-  fi
+  echo "Gather all models on the local machine"
 
-  echo "Install complete"
+  mkdir -p $DIR/http/client/assets
+  ./get_local_models.py $DIR/http/client/assets
+  ./webify_models_v2.py $DIR/http/client/assets
 
-  # Remove temp dir
-  rm -rf $TMP_DIR
-  rm -rf $DIR/http/client/assets
-  mv $DIR/http/client/models $DIR/http/client/assets
+else
+  mkdir -p $DIR/http/client/assets
+  echo "Not cloning the model repo"
 fi
 
-cd $DIR
-
-echo "Gather all models on the local machine"
-
-mkdir -p $DIR/http/client/assets
-./get_local_models.py $DIR/http/client/assets
-./webify_models_v2.py $DIR/http/client/assets
-
-# else
-#  mkdir -p $DIR/http/client/assets
-#  echo "Not cloning the model repo"
-# fi
-
-# if [[ $THUMBNAIL ]]
-# then
-echo "Generating a thumbnail for each model. Make sure gazebo is not running"
-./tools/gzthumbnails.sh
-# fi
+if [[ $THUMBNAIL ]]
+then
+  echo "Generating a thumbnail for each model. Make sure gazebo is not running"
+  ./tools/gzthumbnails.sh
+fi
 
 # build a local model database
-# if [[ $COARSE ]]
-#then
-./coarse_meshes.sh 50 http/client/assets/
-# fi
+if [[ $COARSE ]]
+then
+  ./coarse_meshes.sh 50 http/client/assets/
+fi
 
 echo "Done"
 
